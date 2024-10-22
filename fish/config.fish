199bfc2c660005e3e9d679759979ab0f83e7a7a0
Eric Walker <eric.walker@noldor.com>
2024-10-22 13:56:37 -0600

    Add EDITOR env var

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set XDG_CURRENT_DESKTOP Sway
+    set EDITOR vim
+
     fish_vi_key_bindings
     fish_git_prompt
     set -g __fish_git_prompt_showdirtystate 1
